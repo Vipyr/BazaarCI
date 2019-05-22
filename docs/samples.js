e9052e48f2d42a96108041246afeb82ebe7e5c3d
Tom Manner <tsmanner@us.ibm.com>
2019-05-22 15:37:56 -0400

    Renamed sample1.py to simple_graph.py and added a header for it to the index.html.

--- docs/samples.js
+++ docs/samples.js
@@ -2,12 +2,12 @@ main()
 function main() {
   var result = null;
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "sample1.py", false);
+  xmlhttp.open("GET", "simple_graph.py", false);
   xmlhttp.overrideMimeType("text/plain")
   xmlhttp.send();
   if (xmlhttp.status==200) {
     result = xmlhttp.responseText;
-    var sample1 = document.getElementById("Sample1")
+    var sample1 = document.getElementById("SimpleGraph")
     sample1.textContent = result
     sample1.className = "language-python"
   }
--- docs/simple_graph.py
+++ docs/simple_graph.py
@@ -1,10 +1,11 @@
-from bazaarci.runner import Graph, Step
+from bazaarci.runner import Graph, Product, Step
 
 g = Graph("g")
+p = Product("p")
 s1 = Step("Step1", g, target=lambda: print("Step1"))
 s2 = Step("Step2", g, target=lambda: print("Step2"))
 
-s1.produces("p")  # Step 1 Produces "p"
-s2.consumes("p")  # Step 2 Consumes "p"
+s1.produces(p)  # Step 1 Produces p
+s2.consumes(p)  # Step 2 Consumes p
 
 g.start()  # Begin Thread Execution
